c8a56c65ef5f75428311b6c0e7e529657d64a829
Immern <immanumali12@gmail.com>
2025-11-22 09:37:14 +0800

    [ENHANCEMENT] Added timing functionality to the cfile

--- Test.c
+++ Test.c
@@ -1,16 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 extern void asmhello();
+
+float rand_float() {
+    return (float)rand() / (float)RAND_MAX * 5.0f;  
+}
 
 int main()
 {
-	float x1[4] = {1.5, 4.0, 3.5, 2.0};
-	float x2[4] = {3.0, 2.5, 2.5, 1.0};
-	float y1[4] = {4.0, 3.0, 3.5, 3.0};
-	float y2[4] = {2.0, 2.5, 1.0, 1.5};
+	int input;
+	printf("Enter the number of values for each vector: ");
+	scanf("%d", &input);
 	
-//	float* space = (float*)malloc(12*sizeof(float));
+	float *x1 = malloc(input * sizeof(float));
+    float *x2 = malloc(input * sizeof(float));
+    float *y1 = malloc(input * sizeof(float));
+    float *y2 = malloc(input * sizeof(float));
+
+    srand(time(NULL)); 
+    int i;
+    for (i = 0; i < input; i++) {
+        x1[i] = rand_float();
+        x2[i] = rand_float();
+        y1[i] = rand_float();
+        y2[i] = rand_float();
+    }
+    
+//    testing
+	double totalTime = 0.0;
+	double time;
+	double avgTime;
+	clock_t startTime, endTime;
+
+	for(i = 0; i< 30; i++){
+		startTime = clock();
+		asmhello(x1, x2, y1, y2);
+		endTime = clock();
+		
+		time = (double)(endTime-startTime) / CLOCKS_PER_SEC;
+		totalTime += time;
+	}
 	
-	asmhello(x1, x2, y1, y2);
-	
+	avgTime = totalTime/30.0;
+	printf("Average ASM kernel time for n=%d: %f seconds\n", 30, avgTime);
 	return 0;    
 }
