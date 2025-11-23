55e9750313fae9bd5b356556ae2a5a1ba8df28f7
Immern <immanumali12@gmail.com>
2025-11-23 12:31:26 +0800

    WORKING EVERYTHING!!

--- Test.c
+++ Test.c
@@ -32,9 +32,9 @@ void kernel_c_version(float* x1, float* x2, float* y1, float* y2, int input, flo
 	}
 }
 
-double kernel_c_call(float* x1, float* x2, float* y1, float* y2, int input, float* z){
-	double c_totalTime = 0.0;
-	double c_time;
+float kernel_c_call(float* x1, float* x2, float* y1, float* y2, int input, float* z){
+	float c_totalTime = 0.0;
+	float c_time;
 	clock_t c_startTime, c_endTime;
 	int i;
 	for(i = 0; i< 30; i++){
@@ -42,73 +42,79 @@ double kernel_c_call(float* x1, float* x2, float* y1, float* y2, int input, floa
 		kernel_c_version(x1, x2, y1, y2, input, z);
 		c_endTime = clock();
 		
-		c_time = (double)(c_endTime-c_startTime) / CLOCKS_PER_SEC;
+		c_time = (float)(c_endTime-c_startTime) / CLOCKS_PER_SEC;
 		c_totalTime += c_time;
 	}
 	
 	return c_totalTime;
 }
 
-int main()
-{
-	int input;
-	printf("Enter the number of values for each vector: ");
-	scanf("%d", &input);
-	input = pow(2, 26);
-	float *x1 = malloc(input * sizeof(float));
-    float *x2 = malloc(input * sizeof(float));
-    float *y1 = malloc(input * sizeof(float));
-    float *y2 = malloc(input * sizeof(float));
-	float *z = malloc(input * sizeof(float));
-	
-    srand(time(NULL)); 
-    int i;
-    for (i = 0; i < input; i++) {
-        x1[i] = rand_float();
-        x2[i] = rand_float();
-        y1[i] = rand_float();
-        y2[i] = rand_float();
-    }
-    
-/* =================== C KERNEL VERSION ===================*/
-	printf("============================= 30 runs of the same inputs for the C KERNEL Version =============================\n");
-	double c_totalTime;
-	c_totalTime = kernel_c_call(x1, x2, y1, y2, pow(2, 20), z);
-//	double c_totalTime = 0.0;
-//	double c_time;
-//	double c_avgTime;
-//	clock_t c_startTime, c_endTime;
-//	printf("============================= 30 runs of the same inputs for the C KERNEL Version =============================\n");
-//	for(i = 0; i< 30; i++){
-//		c_startTime = clock();
-//		kernel_c_version(x1, x2, y1, y2, input, z);
-//		c_endTime = clock();
-//		
-//		c_time = (double)(c_endTime-c_startTime) / CLOCKS_PER_SEC;
-//		c_totalTime += c_time;
-//	}
-	
-	
-/* =================== ASM KERNEL VERSION ===================*/
-	double asm_totalTime = 0.0;
-	double asm_time;
+float kernel_asm_call(float* x1, float* x2, float* y1, float* y2, int input, float* z){
+	float asm_totalTime = 0.0;
+	float asm_time;
 	clock_t asm_startTime, asm_endTime;
-	printf("============================= 30 runs of the same inputs for the ASM KERNEL Version =============================\n");
+	int i;
 	for(i = 0; i< 30; i++){
 		asm_startTime = clock();
 		asmhello(x1, x2, y1, y2, z, input);
 		asm_endTime = clock();
 		
-		asm_time = (double)(asm_endTime-asm_startTime) / CLOCKS_PER_SEC;
+		asm_time = (float)(asm_endTime-asm_startTime) / CLOCKS_PER_SEC;
 		asm_totalTime += asm_time;
 	}
 	
+	return asm_totalTime;
+}
+
+int main()
+{
+	int inputSize[3] = {20, 24, 26}; // decided to just use these as well for the seeds
 	
-	double c_avgTime;
-	c_avgTime = c_totalTime/30.0;
-	double asm_avgTime;
-	asm_avgTime = asm_totalTime/30.0;
-	printf("Average C kernel time for n=%d: %f seconds\n", 30, c_avgTime);
-	printf("Average ASM kernel time for n=%d: %f seconds\n", 30, asm_avgTime);
+	int inputs[3] = {pow(2, inputSize[0]), pow(2, inputSize[1]), pow(2, inputSize[2])};
+	int out_loop;
+	float c_totalTime[3];
+	float asm_totalTime[3];
+	int i;
+	for(out_loop=0; out_loop<3; out_loop++){
+	
+		float *x1 = malloc(inputs[out_loop] * sizeof(float));
+	    float *x2 = malloc(inputs[out_loop] * sizeof(float));
+	    float *y1 = malloc(inputs[out_loop] * sizeof(float));
+	    float *y2 = malloc(inputs[out_loop] * sizeof(float));
+		float *z_c = malloc(inputs[out_loop] * sizeof(float));
+		float *z_asm = malloc(inputs[out_loop] * sizeof(float));
+		
+	    srand(inputSize[out_loop]); 
+	    for (i = 0; i < inputs[out_loop]; i++) {
+	        x1[i] = rand_float();
+	        x2[i] = rand_float();
+	        y1[i] = rand_float();
+	        y2[i] = rand_float();
+	    }
+	    
+		/* =================== C KERNEL VERSION ===================*/
+		printf("============================= 30 runs of the same inputs for the C KERNEL Version for 2^%d Inputs =============================\n", inputSize[out_loop]);
+		c_totalTime[out_loop] = kernel_c_call(x1, x2, y1, y2, inputs[out_loop], z_c);
+		/* =================== ASM KERNEL VERSION ===================*/
+		printf("============================= 30 runs of the same inputs for the ASM KERNEL Version for 2^%d Inputs =============================\n", inputSize[out_loop]);
+		asm_totalTime[out_loop] = kernel_asm_call(x1, x2, y1, y2, inputs[out_loop], z_asm);
+		
+		free(x1);
+        free(x2);
+        free(y1);
+        free(y2);
+        free(z_c);
+        free(z_asm);
+	}
+	
+	float c_avgTime[3];
+	float asm_avgTime[3];
+		
+	for(i=0; i<3; i++){
+		c_avgTime[i] = c_totalTime[i]/30.0;
+		asm_avgTime[i] = asm_totalTime[i]/30.0;	
+		printf("Average C kernel time for n=%d: %f seconds\n", 30, c_avgTime[i]);
+		printf("Average ASM kernel time for n=%d: %f seconds\n\n", 30, asm_avgTime[i]);
+	}
 	return 0;    
 }
